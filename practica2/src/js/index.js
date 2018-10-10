d7e3512fd22b9b5fa820cc65f8cb95a8d28ecf51
Samanta <slaragiannone@gmail.com>
2018-10-10 16:31:50 +0100

    mejoras en el tamaño

--- practica2/src/js/index.js
+++ practica2/src/js/index.js
@@ -1,19 +1,28 @@
+var boton = document.getElementById("boton");
+boton.onclick = function pulsarBoton() {
+  var valor = _.random(1, 90);//devuelve un número entero entre 1 y 30
+  var dado = document.getElementById("dado");
+  dado.textContent = valor; 
+}
+
 function crearCarton(rango){
   var rango = _.range(1,91);
   var numeros = _.shuffle(rango);
-  var carton = [];
-  for (var i = 0; i < 15; i++){
+  var carton = Array(25);
+  for (var i = 0; i < carton.length; i++){
     var cas = numeros.splice(0,1);
   }
   if (!carton.includes(cas)){
     carton.push(cas);
   }
-  
-  return carton;
+  /*
+  var casilla = document.getElementById('casilla');
+  elemento.innerHTML = 'carton[i]';
+  */
 }
 
 crearCarton(rango);
 
 function jugar(){
-  
+ 
 }
