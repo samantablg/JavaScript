c66fc2103b491651453458567dd887e459ecaeab
Samanta <slaragiannone@gmail.com>
2018-10-10 13:58:00 +0100

    diseño del cartón de bingo y comienzo de las funciones de js

---
+++ practica2/src/js/index.js
@@ -0,0 +1,20 @@
+var rango = _.range(1,91);
+
+function crearCarton(rango){
+  var numeros = _.shuffle(rango);
+  var carton = [];
+  for (var i = 0; i < 15; i++){
+    var cas = numeros.splice(0,1);
+  }
+  if (!carton.includes(cas)){
+    carton.push(cas);
+  }
+  
+  return carton;
+}
+
+crearCarton(rango);
+
+function jugar(){
+  
+}
